c0d8b375259f4a7cb2ab3010d3b86b8c231a691b
iampkuhz <iampkuhz@gmail.com>
2025-12-04 11:17:06 +0800

    CI fix

--- src/level7_force/force.sol
+++ src/level7_force/force.sol
@@ -1,13 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Force { /*
+contract Force {
+    /*
                    MEOW ?
          /\_/\   /
     ____/ o o \
     /~____  =ø= /
     (______)__m_m)
-                   */ }
+                   */
+
+    }
 
 contract ForceDeposit {
     function deposit(address _f) external payable {
